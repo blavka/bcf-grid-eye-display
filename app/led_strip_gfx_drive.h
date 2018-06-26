cd205549bfc41359df83a1754f2452d937104a47
Karel Blavka <karel.blavka@bigclown.com>
2018-06-26 11:33:17 +0200

    Add led strip gfx driver fox 16x16 and 8x8 matrix

--- app/led_strip_gfx_drive.h
+++ app/led_strip_gfx_drive.h
@@ -3,7 +3,9 @@
 
 #include <bc_gfx.h>
 
-extern const bc_gfx_driver_t led_strp_gfx_driver;
+extern const bc_gfx_driver_t led_strp_gfx_driver_16x16;
+
+extern const bc_gfx_driver_t led_strp_gfx_driver_8x8;
 
 #endif // _LED_STRIP_GFX_DRIVER_H
 
--- app/led_strip_gfx_driver.c
+++ app/led_strip_gfx_driver.c
@@ -11,14 +11,8 @@ void clear(void *self)
     bc_led_strip_fill((bc_led_strip_t *)self, 0);
 }
 
-void draw_pixel(void *self, int left, int top, uint32_t color)
+void draw_pixel_16x16(void *self, int left, int top, uint32_t color)
 {
-    // Transparent background
-    if (!color)
-    {
-        return;
-    }
-
     int position = left * 16;
 
     if (left % 2 == 1)
@@ -33,23 +27,55 @@ void draw_pixel(void *self, int left, int top, uint32_t color)
     bc_led_strip_set_pixel((bc_led_strip_t *)self, position , color << 8);
 }
 
+void draw_pixel_8x8(void *self, int left, int top, uint32_t color)
+{
+    int position = left * 8;
+
+    if (left % 2 == 1)
+    {
+        position += 7 - top;
+    }
+    else
+    {
+        position += top;
+    }
+
+    bc_led_strip_set_pixel((bc_led_strip_t *)self, position , color << 8);
+}
+
 bool update(void *self)
 {
     return bc_led_strip_write((bc_led_strip_t *)self);
 }
 
-bc_gfx_caps_t get_caps(void *self)
+bc_gfx_caps_t get_caps_16x16(void *self)
 {
     (void) self;
     static bc_gfx_caps_t size = { .width = 16, .height = 16};
     return size;
 }
 
-const bc_gfx_driver_t led_strp_gfx_driver = {
+bc_gfx_caps_t get_caps_8x8(void *self)
+{
+    (void) self;
+    static bc_gfx_caps_t size = { .width = 8, .height = 8};
+    return size;
+}
+
+const bc_gfx_driver_t led_strp_gfx_driver_16x16 = {
        .is_ready = is_ready,
        .clear = clear,
-       .draw_pixel = draw_pixel,
+       .draw_pixel = draw_pixel_16x16,
        .get_pixel = NULL,
        .update = update,
-       .get_caps = get_caps
+       .get_caps = get_caps_16x16
+};
+
+const bc_gfx_driver_t led_strp_gfx_driver_8x8 = {
+       .is_ready = is_ready,
+       .clear = clear,
+       .draw_pixel = draw_pixel_8x8,
+       .get_pixel = NULL,
+       .update = update,
+       .get_caps = get_caps_8x8
 };
